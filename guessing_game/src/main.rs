0fddd8e101b5c1fd6e30faebf9e4165c2287f8ea
Vajira <vajirawke@gmail.com>
2023-03-15 17:05:21 +0530

    deps and read a number from terminal

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,14 @@
+use core::num;
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome gussing game");
+    //define a string variable to hold read value from terminal
+    let mut number : String = String::new();
+    println!("please enter a  number ");
+
+    // read the nuber from the termnal to number
+    io::stdin().read_line(&mut number).expect("Failed to read line");
+    println!("number is {} ",number);    
+
 }
