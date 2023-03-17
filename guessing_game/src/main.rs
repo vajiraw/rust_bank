e40718055a65ab0fdb7b1550223d8e0b7f2209d7
Vajira <vajirawke@gmail.com>
2023-03-17 10:19:11 +0530

    add comparison to main.rs

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,32 @@
-use core::num;
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Welcome gussing game");
     //define a string variable to hold read value from terminal
-    let mut number : String = String::new();
-    println!("please enter a  number ");
 
-    // read the nuber from the termnal to number
-    io::stdin().read_line(&mut number).expect("Failed to read line");
-    println!("number is {} ",number);    
+    loop {
+        let mut number: String = String::new();
+        let secret_number = rand::thread_rng().gen_range(1..100);
+        println!("generated number {} ", secret_number);
 
+        println!("please enter a  number ");
+        // read the nuber from the termnal to number
+        io::stdin()
+            .read_line(&mut number)
+            .expect("Failed to read line");
+        println!("enter number is {} ", number);
+        // user enter number trim and parse to uint
+        let p: i32 = number.trim().parse().unwrap();
+
+        match secret_number.cmp(&p) {
+            Ordering::Less => println!(" Less "),
+            Ordering::Equal => {
+                println!(" Equals ");
+                break;
+            }
+            Ordering::Greater => println!(" Greater "),
+        }
+    }
 }
